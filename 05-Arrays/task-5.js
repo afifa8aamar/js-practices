f94caebfae03c84bd58654712305da27a2b49e22
afifa8aamar <afifa.aamar@gmail.com>
2019-06-01 01:03:09 +0400

    fix(05-Arrays): Fix code

--- 05-Arrays/task-5.js
+++ 05-Arrays/task-5.js
@@ -1,50 +1,47 @@
-function myReduce (arr, f , red)
-{
+var myReduce = function(arr,f , init) {
+
   if(!Array.isArray(arr)) 
-      throw new TypeError ('Provide only an Array');
-    else if (typeof f !== 'function')
-      throw new TypeError ('Provide a function');
-    else if(arr.length < 1) 
-      throw new TypeError( 'Please provide Array with values ');
-    else if(typeof red !== 'number') 
-      throw new TypeError( 'Please provide a number ');  
-  else {
-    
-      var len = arr.length;
-      var k = 0; 
-      var value = red;
+    throw new TypeError ('Provide only an Array');
+  else if (typeof f !== 'function')
+    throw new TypeError ('Provide a function');
+  else if(arr.length < 1) 
+    throw new TypeError( 'Please provide Array with values ');
+  else if(typeof init !== 'string' && typeof init !== 'number') 
+    throw new TypeError( 'Please provide a string or number');  
 
-        if (arr.length > 0) 
-          {
-              value =red + arr[0];
-          } 
-        else 
-          {
-              while (k < len && !(k in arr)) 
-                {
-                  k++; 
-                }
-                value = red + arr[k++];
-            }
+  var i = 0;
+  var value;
 
-      while (k < len) {
-
-        if (k in arr) {
-          value = f(value, arr[k], k,arr);
-        }
-     
-        k++;
-      }
-      return value-1;
+  if (arr.length > 1) 
+  {
+  if(typeof init == 'string')
+    value = '' + init;
+    else 
+    value = 0 + init;
   }
+  else 
+  {
+    while (i >= 0 && !(i in arr)) 
+    {
+      i++;
+    }
+    value = arr[i++];
+  }
+  for (; i <= arr.length; i++) 
+  {
+    if (i in arr) 
+      value = f(value, arr[i], i, arr);
+  }
+  return value;
 }
 
 
-var result = function(sum, current) {
-  return sum + current;
-};
-var arr = [1, 2, 3, 4];
-var arr1 = [];
-var acc = 10;
-console.log(myReduce(arr, result, acc)); 
+
+var arr = ['a', 'b', 'c'];
+var arr1 = [1,2,3,4];
+var result = myReduce(arr1,function(sum, current) {
+return sum + current;
+} , 0);
+
+console.log(result);
 // console.log(myReduce(arr1, result, acc)); // typeError
