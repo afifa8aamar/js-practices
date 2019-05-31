fc5d4de5345252716e06b3d127d39a539753cfb8
afifa8aamar <afifa.aamar@gmail.com>
2019-05-31 21:36:46 +0400

    feat(05-Arrays): Add task-4

---
+++ 05-Arrays/task-4.js
@@ -0,0 +1,45 @@
+var arr = [1, -1, 2, -2, 3];
+var arr1 = [-1, -2, -3];
+var arr2 = ['hi',-2, 3];
+
+function myEvery (arr, f )
+{
+  if(!Array.isArray(arr)) 
+    throw new Error ('Provide only an Array');
+  else if (!typeof fun == 'function')
+    throw new Error ('Provide a function');
+
+    else 
+    {
+      i = 0;
+
+      while (i < arr.length) 
+      {
+        var value;
+        if (i in arr) 
+        {
+          if (typeof arr[i] != 'number')
+            throw new TypeError('provide only numbers');
+              else 
+              {
+                value = arr[i];
+                var testResult = f.call(arr, value, i, arr);
+                  if (testResult) 
+                  {
+                    return true;
+                  }
+              }
+        }
+        i++;
+      }
+      return false;
+  }
+}
+
+var status = function(n){
+  return n > 0;
+};
+
+console.log(myEvery(arr, status));
+console.log(myEvery(arr1, status));
+// console.log(myEvery(arr2, status)); // TypeError
