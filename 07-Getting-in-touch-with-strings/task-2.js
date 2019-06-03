497ed8a6fab5ca38b59af56bbae2eb11b73bc367
afifa8aamar <afifa.aamar@gmail.com>
2019-06-03 15:22:26 +0400

    feat(07-Getting-in-touch-with-strings): Add task-2

---
+++ 07-Getting-in-touch-with-strings/task-2.js
@@ -0,0 +1,22 @@
+var cehck ; 
+const checkSpam = function (source, example)
+{
+    if (typeof source !== 'string' || typeof example !== 'string')
+        throw new TypeError('Please provide Strings only');
+    
+    var temp;
+    const count = example.length-1;
+    for (var i = 0; i < source.length ; i++)
+    {
+        if (example == source.substring(i,count));
+        {
+                return true;
+        }
+    }
+    return false;
+        
+};
+
+console.log(checkSpam('aaa@lsfjgabsdfs', 'ab'));
+
+
