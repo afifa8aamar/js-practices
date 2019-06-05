c8fe36612a6692e1b944f0362cfa1520e88f64c1
afifa8aamar <afifa.aamar@gmail.com>
2019-06-05 20:43:28 +0400

    fix(09-Expanded-Object-Functionality): change task-1 still in progress

--- 09-Expanded-Object-Functionality/task-1.js
+++ 09-Expanded-Object-Functionality/task-1.js
@@ -1,15 +1,23 @@
+
 Object.prototype.extend = function(o) 
 {
-    for (prop in o) 
+    for (var prop of Object.keys(o))
     {
-        o[prop] = Object[prop] ;
+        if (typeof this[prop] === 'undefined')
+            this[prop] = o[prop];
     }
 }
+
+
+
 
 const data = { a: 'a' };
 const source = { a: 'A', b: 'b' };
 data.extend(source);
 console.log(data);
 
-//Object.defineProperty(source, 'b', { writable: false });
-// console.log(Object.getOwnPropertyDescriptor(source));
+Object.defineProperty(source, 'b', { writable: false });
+Object.defineProperty(Object.prototype, extend, {enumerable: false});
+console.log(Object.getOwnPropertyDescriptor(data, 'b').writable); 
+
+
