cba421356fec6f38a8f8ed39a8b19c06b9895670
afifa8aamar <afifa.aamar@gmail.com>
2019-06-15 14:52:21 +0400

    fix(15-Sets-and-Maps): Fix delete

--- 15-Sets-and-Maps/task-1.js
+++ 15-Sets-and-Maps/task-1.js
@@ -28,7 +28,7 @@ class DB
             throw new TypeError('salary is not defined or not a number');
         }
         this.map.set(obj.name,obj);
-        id = obj.name;
+        var id = obj.name;
         return id;
     }
 
@@ -45,7 +45,7 @@ class DB
 
     readAll()
     {
-        const a = [...this.map.entries()]
+        const a = [...this.map.values()]
         return a;
     }
 
@@ -69,10 +69,27 @@ class DB
             throw new TypeError('salary is not defined or not a number');
         if (typeof obj.name !== 'undefined')
         {
-            console.log(read());
-           // getObj = read(id)
-           // getObj.name = obj.name;
-           // this.map.set(id, getObj);
+            var getObj = this.read(id)
+            getObj.name = obj.name;
+            this.map.set(id, getObj);
+        }
+        if (typeof obj.age !== 'undefined')
+        {
+            var getObj = this.read(id)
+            getObj.age = obj.age;
+            this.map.set(id, getObj);
+        }
+        if (typeof obj.country !== 'undefined')
+        {
+            var getObj = this.read(id)
+            getObj.country = obj.country;
+            this.map.set(id, getObj);
+        }
+        if (typeof obj.salary !== 'undefined')
+        {
+            var getObj = this.read(id)
+            getObj.salary = obj.salary;
+            this.map.set(id, getObj);
         }
         
     }
@@ -80,8 +97,8 @@ class DB
     delete (id)
     {
         if ( this.map.get(id) == void 0)
-            throw new TypeError('This id does not exist!');
-        return this.map.delete(id);
+            throw new ReferenceError('Invalid Id');
+        console.log(this.map.delete(id)) ;
     }
 }
 
@@ -96,7 +113,16 @@ const person = {
 };
 
 const id = db.create(person);
+console.log(id);
 const customer = db.read(id);
+console.log(customer);
 const customers = db.readAll(); // array of users
+console.log(customers);
 db.update(id, { age: 22 }); // id
+console.log('After update: ');
+console.log(customers);
 db.delete(id); // true
+console.log('After delete: ');
+const customers1 = db.readAll();
+console.log(customers1);
+
